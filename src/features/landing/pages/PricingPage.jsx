270fb1ac8d8dcd6cd2e00ab1185cd02498386741
Uzair Khan <uk873195@gmail.com>
2026-05-03 00:20:29 +0530

    add pricing page

--- src/features/landing/pages/PricingPage.jsx
+++ src/features/landing/pages/PricingPage.jsx
@@ -1,7 +1,5 @@
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
 import '../styles/pricing.scss';
 
 const tiers = [
